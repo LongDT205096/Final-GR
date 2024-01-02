3d29d71fdad86b9f23a89f7874ef46409a04cdb5
LongDT205096 <dinhthanhlong0@gmail.com>
2024-01-02 23:33:46 +0700

    init

--- Backend/accounts/models.py
+++ Backend/accounts/models.py
@@ -6,15 +6,26 @@ from django.contrib.auth.models import (
 )
 
 class AccountManager(BaseUserManager):
-    def create_user(self, username, password):
-        user = self.model(username=username)
+    def create_user(self, email, password=None):
+        if not email:
+            raise ValueError("Users must have an email address")
+
+        user = self.model(
+            email=self.normalize_email(email),
+        )
         user.set_password(password)
         user.save(using=self._db)
 
         return user
 
-    def create_superuser(self, username, password):
-        user = self.create_user(username, password)
+    def create_superuser(self, email, password):
+        if not email:
+            raise ValueError("Users must have an email address")
+
+        user = self.model(
+            email=self.normalize_email(email),
+        )
+        user.set_password(password)
         user.is_staff = True
         user.is_superuser = True
         user.save(using=self._db)
@@ -23,31 +34,34 @@ class AccountManager(BaseUserManager):
 
 # Create your models here.
 class Account(AbstractBaseUser, PermissionsMixin):
-    username = models.CharField(max_length=50, unique=True)
-    password = models.CharField(max_length=256)
+    email = models.EmailField(max_length=50, unique=True, blank=True)
+    password = models.CharField(max_length=50)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
-    user_permissions = models.ManyToManyField(
-        "auth.Permission", related_name="custom_user_permissions"
-    )
+    is_superuser = models.BooleanField(default=False)
 
-    USERNAME_FIELD = "username"
+    USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["password"]
 
     objects = AccountManager()
 
     def __str__(self) -> str:
-        return self.username
+        return self.email
+    
+    def has_perm(self, perm, obj=None):
+        return self.is_superuser
+    
+    def has_module_perms(self, app_label):
+        return True
     
     
 class User(models.Model):
     account = models.OneToOneField(Account, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    email = models.EmailField(unique=True)
     phone = models.CharField(max_length=10)
     bio = models.TextField(blank=True)
-    birth_date = models.DateField(null=True, blank=True)
+    birthday = models.DateField(null=True, blank=True)
     country = models.CharField(max_length=50)
     
     def __str__(self) -> str:
