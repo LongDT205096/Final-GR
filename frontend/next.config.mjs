c8ca04b5f4237140fc5715abf4c7b96c53a845d1
LongDT205096 <dinhthanhlong0@gmail.com>
2024-03-01 22:02:23 +0700

    fe như cc

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+    webpack: (config, { isServer }) => {
+      // Add HTML loader only for client-side code
+      if (!isServer) {
+        config.module.rules.push({
+          test: /\.html$/,
+          loader: 'html-loader'
+        });
+      }
+      return config;
+    }
+  };
+  
 export default nextConfig;
+  
---
+++ frontend/src/components/homepage/homepage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Navbar from "../navbar/navbar";
+
+const Homepage = () => {
+  return (
+    <div>
+      <Navbar />
+      <h1>Homepage</h1>
+    </div>
+  );
+};
